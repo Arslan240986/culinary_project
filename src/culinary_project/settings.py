80c63e98bb148c3a9ef007d92a56351c0f726089
arslans-repo <arslan092486@gmail.com>
2021-04-10 14:11:34 +0500

    deploy 5

--- src/culinary_project/settings.py
+++ src/culinary_project/settings.py
@@ -122,6 +122,12 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'media_root')
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'arslan092486@gmail.com'
+EMAIL_HOST_PASSWORD = '240986arslaN'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # redis settings
