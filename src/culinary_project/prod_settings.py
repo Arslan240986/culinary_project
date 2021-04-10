17920ee30a44224b68169f2b34b22518fec4d50e
arslans-repo <arslan092486@gmail.com>
2021-04-10 10:18:08 +0500

    deploy 4

--- src/culinary_project/prod_settings.py
+++ src/culinary_project/prod_settings.py
@@ -5,22 +5,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = '80s84g@6^1t)lvtshvdjejckab663384bbcj8ba)fuhgudpa&0bt)j*ju8py1'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1', '5.101.5.119', 'awchy.ru']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'culinary',
-        'USER': 'arslanUSh',
+        'NAME': 'culinary_project',
+        'USER': 'arslan_ushefa',
         'PASSWORD': 'arslan240986',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '5422',
     }
 }
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_project',)
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static_project',)
+# ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'static_root')
