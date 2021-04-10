33996d842dc2d13ca986f09655e8d9cb4923e8ee
arslans-repo <arslan092486@gmail.com>
2021-04-10 11:45:53 +0500

    deploy 5

--- src/culinary_project/prod_settings.py
+++ src/culinary_project/prod_settings.py
@@ -20,7 +20,7 @@ DATABASES = {
     }
 }
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static',)
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static',)
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'static_root')
