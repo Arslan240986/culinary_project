3d7402370e0025b5636acf321224637c66441504
arslans-repo <arslan092486@gmail.com>
2021-04-11 11:21:24 +0500

    deploy 6

--- src/contact/signals.py
+++ src/contact/signals.py
@@ -8,7 +8,6 @@ from private_chat.models import Thread
 @receiver(post_save, sender=User)
 def post_save_create_profile(sender, instance, created, **kwargs):
     if created:
-        print(instance.username)
         profile = UserProfile.objects.create(user=instance, email=instance.email)
         if instance.username:
             profile.first_name = instance.username
