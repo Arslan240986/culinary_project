ecee50a7adf87fe9e15742a0596d2acd57c78ff1
arslans-repo <arslan092486@gmail.com>
2021-04-13 12:06:01 +0500

    deploy-10

--- src/culinary_project/prod_settings.py
+++ src/culinary_project/prod_settings.py
@@ -5,30 +5,30 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = '80s84g@6^1t)lvtshvdjejckab663384bbcj8ba)fuhgudpa&0bt)j*ju8py1'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '5.101.5.119', 'awchy.ru']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'culinary_project',
-        'USER': 'arslan_ushefa',
-        'PASSWORD': 'arslan240986',
-        'HOST': '127.0.0.1',
-        'PORT': '5422',
-    }
-}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'culinary',
-#         'USER': 'postgres',
-#         'PASSWORD': '12345qw',
+#         'NAME': 'culinary_project',
+#         'USER': 'arslan_ushefa',
+#         'PASSWORD': 'arslan240986',
 #         'HOST': '127.0.0.1',
 #         'PORT': '5422',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'culinary',
+        'USER': 'postgres',
+        'PASSWORD': '12345qw',
+        'HOST': '127.0.0.1',
+        'PORT': '5422',
+    }
+}
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static',)
