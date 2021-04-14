6eafe051451d4413df0489fd1aeebe1a2e39ae2c
arslans-repo <arslan092486@gmail.com>
2021-04-15 00:12:46 +0500

    deploy-migra

--- src/culinary_project/prod_settings.py
+++ src/culinary_project/prod_settings.py
@@ -12,8 +12,8 @@ ALLOWED_HOSTS = ['127.0.0.1', '5.101.5.119', 'ushefa.ru']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'culinary_project',
-        'USER': 'arslan_ushefa',
+        'NAME': 'ushefa',
+        'USER': 'arslan_chef',
         'PASSWORD': 'arslan240986',
         'HOST': '127.0.0.1',
         'PORT': '5422',
