9fb9bab2c4449e28b8a6c565ac73b7c2017a3c5e
arslans-repo <arslan092486@gmail.com>
2021-04-18 10:54:06 +0500

    gmail accaunt

--- src/culinary_project/prod_settings.py
+++ src/culinary_project/prod_settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = '80s84g@6^1t)lvtshvdjejckab663384bbcj8ba)fuhgudpa&0bt)j*ju8py1'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '5.101.5.119', 'ushefa.ru']
 
@@ -36,10 +36,11 @@ if DEBUG:
     ]
 else:
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
+        os.path.join(BASE_DIR, 'static'),
     ]
-    STATIC_ROOT = os.path.join(os.path.join(BASE_DIR), 'static_cdn/', 'static_root/')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn/', 'static_root/')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static',)
 # ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'static_root')
+print(BASE_DIR)
