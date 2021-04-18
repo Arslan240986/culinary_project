5e60162574661bd8d4802c8262dbc7e723d9cb0a
arslans-repo <arslan092486@gmail.com>
2021-04-18 11:49:14 +0500

    gmail accaunt

--- src/culinary_project/prod_settings.py
+++ src/culinary_project/prod_settings.py
@@ -9,26 +9,26 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '5.101.5.119', 'ushefa.ru']
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ushefa',
-#         'USER': 'arslan_chef',
-#         'PASSWORD': 'arslan240986',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5422',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'culinary',
-        'USER': 'postgres',
-        'PASSWORD': '12345qw',
+        'NAME': 'ushefa',
+        'USER': 'arslan_chef',
+        'PASSWORD': 'arslan240986',
         'HOST': '127.0.0.1',
         'PORT': '5422',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'culinary',
+#         'USER': 'postgres',
+#         'PASSWORD': '12345qw',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5422',
+#     }
+# }
 # if DEBUG:
 #     STATIC_DIR = os.path.join(BASE_DIR, 'static')
 #     STATICFILES_DIRS = [
