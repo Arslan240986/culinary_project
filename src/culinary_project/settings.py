0badc18924b2223492f52c1e5d416a2e65245ba1
arslans-repo <arslan092486@gmail.com>
2021-04-19 11:36:08 +0500

    gmail accaunt2

--- src/culinary_project/settings.py
+++ src/culinary_project/settings.py
@@ -117,13 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static',)
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn/', 'static_root/')
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [STATIC_DIR,]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn/', 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media')
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ushefa1ru@gmail.com'
