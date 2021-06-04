651e70d02a5c6d904148c88db08108fcf9ccd8c3
arslans-repo <arslan092486@gmail.com>
2021-06-04 17:36:50 +0500

    mac_in_job

--- src/culinary_recipe/forms.py
+++ src/culinary_recipe/forms.py
@@ -59,7 +59,7 @@ class DishForm(forms.ModelForm):
             'preparation_time_minute': forms.NumberInput(attrs={'placeholder': 'минуты'}),
             'cooking_time_hour': forms.NumberInput(attrs={'placeholder': 'часы'}),
             'cooking_time_minute': forms.NumberInput(attrs={'placeholder': 'минуты'}),
-            'servings': forms.CharField(attrs={'style': 'width: 100%;'}),
+            'servings': forms.TextInput(attrs={'style': 'width: 100%;'}),
             'technology': forms.SelectMultiple(attrs={'multiple':"",
                                                       'class':"ui fluid dropdown",
                                                       'placeholder': 'Не выбрано'}),
