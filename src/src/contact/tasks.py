de771f7e862910fe16f2a01714d8b3714ca42feb
arslans-repo <arslan092486@gmail.com>
2021-06-06 14:11:36 +0500

    my_bad

---
+++ src/src/contact/tasks.py
@@ -0,0 +1,47 @@
+from culinary_project.celery import app
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+
+from culinary_project import settings
+
+from contact.models import UserProfile
+
+
+@app.task
+def send_subscribe(email):
+    subject = 'Подписка на рассылку'
+    message = 'Вы успешно подписались на рассылку на сайте У-шефа'
+
+    mail_sent = send_mail(subject,
+                          message,
+                          'admin@myshop.com',
+                          [email])
+    return mail_sent
+
+
+@app.task
+def send_success_subscribe(**kwargs):
+    author = User.objects.get(id=kwargs['author'])
+    send_mail(
+        'Успешно пройдена проверка',
+        f'Дорогой {author.profile.first_name} Ваш рецепт {kwargs["title"]} упешно прошла проверку и добавлен на сайт',
+        settings.EMAIL_HOST_USER,
+        [author.profile.email],
+        fail_silently=False
+    )
+
+
+@app.task
+def notice_about_new_messages(**kwargs):
+    sender = UserProfile.objects.get(id=kwargs['sender'])
+    receiver = UserProfile.objects.get(id=kwargs['receiver'])
+    print(kwargs)
+    mail_sent = send_mail(
+        'Успешно пройдена проверка',
+        f'Дорогой {receiver.first_name} Вам  {sender.first_name} на сайте У-шефа отправил сообшения для прочтения пройдите пожалуйста по ссылке\n https://ushefa.ru{receiver.get_absolute_url_for_friends_view()}',
+        settings.EMAIL_HOST_USER,
+        [receiver.email],
+        fail_silently=False
+    )
+    return mail_sent
+
---
+++ src/src/culinary_post/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'culinary_post'
+
+urlpatterns = [
+    path('', views.post__list_view, name='culinary_post_view'),
+    path('detail/<int:pk>', views.CulinaryPostDetailView.as_view(), name='culinary_post_detail_view'),
+    path("detail/<int:pk>/<int:number>/", views.CulinaryPostDetailView.as_view(), name='load_more_comments'),
+    path('post/add_comment/<int:pk>', views.AddCommentToPost.as_view(), name='post_add_comment'),
+    path('post_add/', views.posts_add, name='culinary_post_add'),
+    path('liked', views.like_unlike_post, name='like_post_view'),
+    path('<pk>/delete/', views.CulinaryPostDeleteView.as_view(), name='post_delete'),
+    path('<pk>/update/', views.CulinaryPostUpdateView.as_view(), name='post_update'),
+]
