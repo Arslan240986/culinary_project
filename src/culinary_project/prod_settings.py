c2e5b0e6a8675c3a71e58f5617ae85282cc7978a
arslans-repo <arslan092486@gmail.com>
2021-06-18 17:28:18 +0500

    some_frontend

--- src/culinary_project/prod_settings.py
+++ src/culinary_project/prod_settings.py
@@ -5,10 +5,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = '80s84g@6^1t)lvtshvdjejckab663384bbcj8ba)fuhgudpa&0bt)j*ju8py1'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '5.101.5.119', 'ushefa.ru']
-DATABASES = {
+if not DEBUG:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'ushefa_ru',
@@ -18,5 +19,15 @@ DATABASES = {
             'PORT': '5422',
         }
     }
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'culinary_rewrite',
+            'USER': 'postgres',
+            'PASSWORD': '12345qw',
+            'HOST': '127.0.0.1',
+            'PORT': '5422',
+        }
+}
 
