d98699e98846cb9da30c0c3c1a2b67e2be608aba
arslans-repo <arslan092486@gmail.com>
2021-08-21 18:34:11 +0500

    remove list of ingredient

--- src/culinary_project/prod_settings.py
+++ src/culinary_project/prod_settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = '80s84g@6^1t)lvtshvdjejckab663384bbcj8ba)fuhgudpa&0bt)j*ju8py1'
-
+# aaaaa
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '5.101.5.119', 'ushefa.ru']
