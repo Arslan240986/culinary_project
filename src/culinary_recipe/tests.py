a9b6851a2bfa7b3a662a6389f91eed0c349c23a7
arslans-repo <arslan092486@gmail.com>
2021-11-20 13:00:32 +0500

    working on masonry

--- src/culinary_recipe/tests.py
+++ src/culinary_recipe/tests.py
@@ -1,4 +1,26 @@
-arslan =  'Крокеты из индейки и картофельного пюре 7_1_11zon.jpeg'
-newargs = arslan.split('.')
-print(newargs[0])
+a = range(1, 10001)
+
+# for b in a:
+#     if b%2 != 0:
+#         print(b)
+
+def get_simple(a):
+    answer = list()
+    for numbers in range(1, a+1):
+        if a % numbers == 0:
+            answer.append(numbers)
+    return answer
+    # if len(answer) >= 1:
+    #     pass
+    # else:
+    #     return a
+print(get_simple(500))
+print(1965/131)
+# arr = list()
+# for b in a:
+#     num = get_simple(b)
+#     if type(num) is int:
+#         arr.append(num)
+
+# print(arr)
 
