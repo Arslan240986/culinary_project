d4db8677f158cc9b4d0d53e8f60aae92f2494abe
arslans-repo <arslan092486@gmail.com>
2022-04-07 13:40:45 +0500

    change ip

--- src/culinary_project/prod_settings.py
+++ src/culinary_project/prod_settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = '80s84g@6^1t)lvtshvdjejckab663384bbcj8ba)fuhgudpa&0bt)j*ju8py1'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '5.101.5.119', 'ushefa.ru', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '146.185.235.119', 'ushefa.ru', 'localhost']
 if not DEBUG:
     DATABASES = {
         'default': {
