48e6ad5171e4d3c24773c0efdefdc891a6308c43
arslans-repo <arslan092486@gmail.com>
2022-04-15 11:42:30 +0500

    fix production

--- src/culinary_project/prod_settings.py
+++ src/culinary_project/prod_settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = '80s84g@6^1t)lvtshvdjejckab663384bbcj8ba)fuhgudpa&0bt)j*ju8py1'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '146.185.235.119', 'ushefa.ru', 'localhost']
 if not DEBUG:
